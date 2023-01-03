26c15dc4d3dcca49badb62be4abf9961f1a411cc
Mostafa Abdelhafez <mostafa.aladar@jobzella.com>
2023-01-04 01:12:49 +0200

    Initial Commit

---
+++ Auction/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Auction
+//
+//  Created by jobzella on 04/01/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
